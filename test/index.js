912808c24c89e0314132caaf197fceafc6ee2b7e
porsager <rasmus@porsager.com>
2014-01-28 09:12:26 +0100

    v0.1.3 added connected event

--- test/index.js
+++ test/index.js
@@ -9,74 +9,77 @@ bl.on('error', function(err) {
   console.log(err);
 });
 
-if(!bl)
-  return console.log('no busylight found');
+bl.once('connected', function(){
 
-bl.defaults({
-  volume: 1,
-  color: 'yellow'
+  if(!bl)
+    return console.log('no busylight found');
+
+  bl.defaults({
+    volume: 1,
+    color: 'yellow'
+  });
+
+  testLight();
+  testRing();
+
+  function testLight() {
+    for(var i = 0; i <= colors.length; i++) {
+      setTimeout(function(color){
+        if(color)
+          bl.light(color);
+        else
+          testBlink();
+      }, 2000*i, colors[i]);
+    }
+  }
+
+  function testRing(){
+    for(var i = 0; i < tones.length; i++) {
+      setTimeout(function(tone){
+        bl.ring(tone);
+      }, 4000*i, tones[i]);
+    }
+  }
+
+  function testBlink() {
+    var s = 4000;
+    bl.blink();
+    setTimeout(function(){
+      bl.blink('red');
+      setTimeout(function(){
+        bl.blink(colors);
+        setTimeout(function(){
+          bl.blink(colors, 100);
+          setTimeout(function(){
+            testPulse();
+          }, s);
+        }, s);
+      }, s);
+    }, s);
+  }
+
+  function testPulse() {
+    var s = 4000;
+
+    bl.pulse();
+    setTimeout(function(){
+      bl.pulse('red');
+      setTimeout(function(){
+        bl.pulse(colors);
+        setTimeout(function(){
+          bl.pulse(colors, 100);
+          setTimeout(function(){
+            close();
+          }, s);
+        }, s);
+      }, s);
+    }, s);
+  }
+
+  function close() {
+    bl.off();
+    setTimeout(function(){
+      bl.close();
+    }, 4000);
+  }
 });
-
-testLight();
-testRing();
-
-function testLight() {
-  for(var i = 0; i <= colors.length; i++) {
-    setTimeout(function(color){
-      if(color)
-        bl.light(color);
-      else
-        testBlink();
-    }, 2000*i, colors[i]);
-  }
-}
-
-function testRing(){
-  for(var i = 0; i < tones.length; i++) {
-    setTimeout(function(tone){
-      bl.ring(tone);
-    }, 4000*i, tones[i]);
-  }
-}
-
-function testBlink() {
-  var s = 4000;
-  bl.blink();
-  setTimeout(function(){
-    bl.blink('red');
-    setTimeout(function(){
-      bl.blink(colors);
-      setTimeout(function(){
-        bl.blink(colors, 100);
-        setTimeout(function(){
-          testPulse();
-        }, s);
-      }, s);
-    }, s);
-  }, s);
-}
-
-function testPulse() {
-  var s = 4000;
-
-  bl.pulse();
-  setTimeout(function(){
-    bl.pulse('red');
-    setTimeout(function(){
-      bl.pulse(colors);
-      setTimeout(function(){
-        bl.pulse(colors, 100);
-        setTimeout(function(){
-          close()
-        }, s);
-      }, s);
-    }, s);
-  }, s);
-}
-
-function close() {
-  bl.off();
-  setTimeout(function(){
-    bl.close();
-  }, 4000);
-}
